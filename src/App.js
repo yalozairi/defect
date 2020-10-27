9239aa6b0e709b2915148bc19fb88ba757e96e3c
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-10-28 00:35:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
+import defectLogoSpin from "./media/DefectLogoSpin.mp4"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ReactPlayer url={defectLogoSpin} width='100%' height='100%' playing={true} loop={true} pip={false}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
