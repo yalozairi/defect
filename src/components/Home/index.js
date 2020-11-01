bfee32bea7ffee6312db757d3c23dc6d37b3d09e
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-11-01 22:00:40 +0300

    Inserted Description

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,13 +2,38 @@ import React from "react";
 // import logo from "../../logo.svg";
 import defectLogoSpin from "../../media/DefectLogoSpin.gif";
 import collage from "../../media/Defect Collage.png";
-import { Collage /*CollageTicker*/, SpinningLogo } from "./styles";
+import {
+  Collage /*CollageTicker*/,
+  Description,
+  DescriptionDiv,
+  SpinningLogo,
+} from "./styles";
 // import Ticker from "react-ticker";
 
 const Home = () => {
   return (
     <div>
       <SpinningLogo src={defectLogoSpin} alt="Defect Logo" />
+      <DescriptionDiv>
+        <Description>
+          Defect is a non-profit organisation that has built it’s own brand to
+          support an important cause. What started as a passion project by
+          Kuwaiti Youth is now a movement that inspires and motivates teenagers
+          to pursue their dreams. By collaborating with local businesses and
+          bringing together people to socialize, Defect has created a community
+          that bonds on mutual support and appreciation of culture, as well as
+          local talent. The word Defect is defined as “a shortcoming or
+          imperfection”. As what adults consider “defects” of society, our
+          supposedly lazy, technology-obsessed generation will come together to
+          show the world what we can do.
+        </Description>
+        <Description>
+          The idea is simple: To bring the products of young, local
+          entrepreneurs to the youth in a social setting and use a contribution
+          of the proceeds to fund further events and expansion. All for the
+          youth!
+        </Description>
+      </DescriptionDiv>
       <Collage src={collage} alt="Defect collage" />
 
       {/* <h1>hhii</h1>
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -11,3 +11,16 @@ export const CollageTicker = styled.div`
 export const SpinningLogo = styled.img`
   width: 100%;
 `;
+
+export const Description = styled.h6`
+  color: ${(props) => props.theme.lightGreen};
+  margin-bottom: 20px;
+  text-indent: 20px;
+`;
+
+export const DescriptionDiv = styled.div`
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 35px;
+`;
